ac913c5397de803c0eee7a0d55c0feb121382a81
Maxence Larrieu <m@larri.eu>
2021-01-10 16:35:48 +0100

    use issn_l to index

--- apc_journal_tables/get_journal_apc_info.py
+++ apc_journal_tables/get_journal_apc_info.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 """
-STEP 1 from openapc database  produce table with year, journals, and average of apc that have been payed 
+STEP 1 from openapc database extract every journals with the amount paid in APC for each years.
 STEP 2 if apc in 2015 and in 2017 and no value in 2016 then fill 2016 with 2015 value (assuming a journal cannot flip 2 times less than 5 years)
 """
 
@@ -18,14 +18,14 @@ print(f"nb of articles w APC {len(df.index)}")
 #print(len(snapshot))
 
 df = df.loc[ df["is_hybrid"] == False]
-issns_a = df["issn"].tolist()
-issn_price = dict.fromkeys(issns_a) #remove duplicate
+issns_l = df["issn_l"].tolist()
+issn_price = dict.fromkeys(issns_l) #remove duplicate
 print(f"nb of non hybrid journals finded\t{len(issn_price)}\n\n")
 
 for i, item in enumerate(issn_price) : 
 	issn_price[item] = []
 	for year in range(2015, 2021):
-		mean = df.loc[ (df["issn"] == item) & (df["period"] == year), "euro" ].mean()
+		mean = df.loc[ (df["issn_l"] == item) & (df["period"] == year), "euro" ].mean()
 		issn_price[item].append(mean)
 	
 	#if i > 20 : break
@@ -33,16 +33,17 @@ for i, item in enumerate(issn_price) :
 	#print(f"the apc average for this issn {item}  is {mean}€")
 
 out = pd.DataFrame.from_dict(issn_price, orient='index')
-out.to_csv("./source/temp_journals_in_openapc.csv") # !!caution!! the issn column is the index :/
+out.to_csv("./source/temp_journals_in_openapc.csv") # !!caution!! the issn_l column is the index
 exit()
 """
 
 
 #____STEP 2____ fill NA values if appropriate
-df = pd.read_csv("./source/temp_openapc_journals.csv")
+df = pd.read_csv("./source/temp_journals_in_openapc.csv")
+
 # overwrite columns name
 temp_col = [str(y) for y in range(2015, 2021)]
-col = ["issn"]
+col = ["issn_l"]
 col.extend(temp_col) 
 df.columns = col
 #print(col)
@@ -78,28 +79,33 @@ for index, val in data.items() :
 		for year in nan_idx : 
 			if year > bottom_val and year < up_val : 
 				val[str(year)] = bottom_val
+				#print(f"{year} mean is added {val}")
 				
 				
 	#if index > 20 : break;
-
 # view piece of data
-for index, val in data.items() : 
+"""for index, val in data.items() : 
 	print(index, val)
-	if index > 5 : break
-
+	if index > 5 : break"""
 
 journals = pd.DataFrame.from_dict(data, orient="index")
 
 #ajout des colonnes issn_print et issn_electronic et journal_title
-openapc = pd.read_csv("./source/openapc.csv", usecols=["issn", "issn_print", "issn_electronic", "journal_full_title"])
-openapc.drop_duplicates(subset="issn", inplace = True)
-journals = journals.merge(openapc, how="left", on= "issn")
+openapc = pd.read_csv("./source/openapc.csv", usecols=["issn_l", "issn", "issn_print", "issn_electronic", "journal_full_title"])
+openapc.drop_duplicates(subset="issn_l", inplace = True)
+journals = journals.merge(openapc, how="left", on= "issn_l")
+#print(journals.info())
+
 
 #reorder columns
 temp_col.insert(0, "journal_full_title")
-temp_col.extend(["issn", "issn_print", "issn_electronic"])
+temp_col.extend(["issn", "issn_print", "issn_electronic", "issn_l"])
 journals = journals[temp_col] 
 journals.to_csv("openapc_journals.csv", index=False)
 
 
 
+
+
+
+#2290
